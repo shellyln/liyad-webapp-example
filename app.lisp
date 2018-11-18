a49484f27a4352ba86636dca96617ca7780f81ac
shellyln <shellyln@users.noreply.github.com>
2018-11-18 20:38:23 +0900

    Edit example

--- app.lisp
+++ app.lisp
@@ -45,22 +45,39 @@
 ;; It need LSX profile and bootstrap javascript file.
 (::srv:#get "/lsx" (-> (req res)
     ($let u (::url:parse ::req:url))
-    ($then ($resolve-pipe (::db:query "")
-        (|-> (data) use (req res u) ($render
+    ($let q-res (#)) ($let q-res-push
+        (|-> (name) use (q-res)
+            (|-> (data) use (q-res name)
+                ($set (q-res ($eval name)) data) ($last data) )))
+    ($then ($resolve-pipe
+        ;; Fetch data
+        (::db:begin)
+        (::db:query "select * from x") (q-res-push "r1")
+        (::db:query "select * from y") (q-res-push "r2")
+        (::db:query "select * from z") (q-res-push "r3")
+        (|-> (data) use (req res u q-res) ($render
             ;; LSX notation
             (page-header-footer "Welcome to LSX example"
                 (div (h1 (Hello (@ (name ::u:path))))
                     (p ($datetime-to-iso-string ($now)))
                     """p@{(style (color "red"))}
                     Good morning.
-                    %%%(h4 data)
+                    %%%(h4 "last query result: " data)
+                    %%%(h4 "all results: " ($json-stringify q-res))
                     """ ))
-            ;; Process the rendering result.
+            ;; Renderer
             (|-> (e html) use (req res)
-                (output-render-result e html req res) ))))
+                ;; Finalize date
+                ($then ($if html (::db:commit) (::db:rollback))
+                    ;; Process the rendering result.
+                    (|-> ()    use (e html req res) (output-render-result e           html req res))
+                    (|-> (err) use (e html req res) (output-render-result ($or e err) html req res)) ) ))))
         (-> () nil)
         (|-> (e) use (req res)
-            (output-render-result e null req res)) )))
+            ($then (::db:rollback)
+                ;; Render the error.
+                (|-> ()    use (e req res) (output-render-result e null req res))
+                (|-> (err) use (e req res) (output-render-result e null req res)) )) )))
 
 
 ;; Start server.
--- db.js
+++ db.js
@@ -3,9 +3,38 @@
 function query(q) {
     return (new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve('this is queried data.')
+            resolve(`this is queried data for query ${q}.`)
         }, 30);
     }));
 }
-
 exports.query = query;
+
+
+function begin() {
+    return (new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('begin')
+        }, 30);
+    }));
+}
+exports.begin = begin;
+
+
+function commit() {
+    return (new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('commit')
+        }, 30);
+    }));
+}
+exports.commit = commit;
+
+
+function rollback() {
+    return (new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('rollback')
+        }, 30);
+    }));
+}
+exports.rollback = rollback;
