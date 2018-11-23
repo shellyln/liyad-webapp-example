e8011292f3947c55b2cd59f8660bf72c7af3b600
shellyln <shellyln@users.noreply.github.com>
2018-11-23 11:23:03 +0900

    Fix example for liyad-cli breaking change v0.0.17.

--- cluster.js
+++ cluster.js
@@ -1,7 +1,7 @@
 
-
+const fs = require('fs');
 const parseArgs = require('liyad-cli').parseArgs;
 const cliCore = require('liyad-cli').cliCore;
 
 console.dir(require('liyad-cli'));
-cliCore(parseArgs(['-p', 'LSX_async', '--lsx-boot', 'lsxboot.js', 'app.lisp']));
+cliCore(fs.realpathSync(process.cwd()), parseArgs(['-p', 'LSX_async', '--lsx-boot', 'lsxboot.js', 'app.lisp']));
